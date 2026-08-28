462bc1bdd0b62655ed9a44162eee9aae703929f4
MIKI4222 <101790029+MIKI4222@users.noreply.github.com>
2026-08-28 19:11:53 +0300

    Update genlayerClient.js

--- src/genlayerClient.js
+++ src/genlayerClient.js
@@ -2,7 +2,7 @@ import { createClient } from 'genlayer-js'
 import { testnetBradbury } from 'genlayer-js/chains'  // ← было testnetAsimov!
 import { custom } from 'viem'
 
-export const CONTRACT_ADDRESS = '0xaDC75AeC2155a4d07aBc38B6356B6dfF58f8c6fb'
+export const CONTRACT_ADDRESS = '0x30d00D02ED527cf4a5d227887F7f62Eb3d580bF6'
 
 export const explorerTxUrl = (hash) =>
   `https://explorer-bradbury.genlayer.com/tx/${hash}`
